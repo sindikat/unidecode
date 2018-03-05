880e8706f31991a445aaf81662ded9edcec62299
John Mastro <john.b.mastro@gmail.com>
2018-03-05 13:49:53 -0800

    License under GPLv2

--- unidecode.el
+++ unidecode.el
@@ -9,7 +9,17 @@
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
-;; This file is placed in the public domain to the extent possible by law.
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, version 2.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
